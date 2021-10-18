25c4a120d83117e2c94f8e82c3d39fae04740c02
Miggy Pinaroc <lorenzo.pinaroc@paymaya.com>
2021-10-18 10:19:04 +0800

    fixed js dependencies and added additional files and directories

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import NotFound from "../components/404.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    // component: () =>
+    //   import("../views/About.vue")
+  },
+  {
+    path: "*",
+    name: "404",
+    component: NotFound
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>hello world</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+h1 {
+    color: $blek;
+}
+</style>
