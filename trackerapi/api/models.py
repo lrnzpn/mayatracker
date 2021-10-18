cdb06739e6e4988a1629881079a3067ca1cea94c
Jerahmeel Chua <jerahmeel.chua@paymaya.com>
2021-10-18 11:36:50 +0800

    Added transactions model, serializer and endpoint

--- trackerapi/api/models.py
+++ trackerapi/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Transaction(models.Model):
+    description = models.CharField(max_length=100)
+    amount = models.FloatField()
+    transactionDate = models.DateField()
+
+    def __str__(self):
+        return self.description
--- trackerapi/api/views.py
+++ trackerapi/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import TransactionSerializer
+from .models import Transaction
 
 # Create your views here.
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all().order_by('id')
+    serializer_class = TransactionSerializer
