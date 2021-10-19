8e0f1608f5b1bf64c7a18dbd1cde4593dcc1f264
Jerahmeel Chua <jerahmeel.chua@paymaya.com>
2021-10-19 18:53:50 +0800

    Added LOGIN_REDIRECT_URL in settings.py

--- trackerapi/trackerapi/settings.py
+++ trackerapi/trackerapi/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/api/v1/'
