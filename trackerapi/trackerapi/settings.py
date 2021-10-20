26a3f524645bdf45b4838f75ae46ae0ce9a05a92
Miggy Pinaroc <lorenzo.pinaroc@paymaya.com>
2021-10-20 17:00:43 +0800

    added env for django

--- trackerapi/trackerapi/settings.py
+++ trackerapi/trackerapi/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import environ
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s6%0&+0%pe$02$z@c05^qb+p9kxf^xr#w%duv(m7#c$u5&=dof'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
